4e9b0046898680c5c09f2ba394c31b9494946763
Nikhil_Desai <112999524+desainikhil03@users.noreply.github.com>
2024-07-01 12:20:34 +0000

    container config done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,19 @@
+const accountId = 22334555
+let accountEmail = "nikhil@gmail.com"
+var accountPassword = "12345"
+accountCity = "Mumbai"
+let accountState  // if not inisilize variavle then it print undifined
+
+//accountId = 23
+accountEmail = "nicks@gmail.com"
+accountPassword = "2456676445"
+accountCity = "pune"
+
+console.log(accountId);
+
+/*
+Prefer not to use var
+because of issue in block scope and functional scope
+*/ 
+
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
