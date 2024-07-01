502c7849419bbb52b0d77b9cd926615b6014b444
Nikhil_Desai <112999524+desainikhil03@users.noreply.github.com>
2024-07-01 13:17:42 +0000

    dataTypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,35 @@
+"use strict";  // treat all JS code as newer  version
+
+//alert(2+3) we are using nodejs , not browser
+
+console.log(3 
+    + 3)           // code readability should bi high
+
+
+console.log("NIKHIL");    
+
+let name = "Nikhil"
+let age = 21
+let isLoggedIn = false
+
+
+
+
+// data types 
+/*
+
+primitive data types 
+number => (range) 2 to power 53
+bigint
+String => ""
+null => standalone value  
+undefined => 
+symbol => unique
+
+*/
+
+// object 
+
+console.log(typeof null);  // object
+
+console.log(typeof undefined); // undifined
