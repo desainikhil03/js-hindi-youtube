18f78aaa2180e5669a8d96e478330192485bbf33
Nikhil_Desai <112999524+desainikhil03@users.noreply.github.com>
2024-07-08 12:30:18 +0000

    maths and nums

---
+++ 01_basics/06_nums_and_maths.js
@@ -0,0 +1,33 @@
+const score = 400
+//console.log(score);
+
+const balance = new Number(100)
+// console.log(balance);
+
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(2));
+
+const anotherNumber = 123.8966
+// console.log(anotherNumber.toPrecision(3));
+
+const hundreds = 1000000
+//console.log(hundreds.toLocaleString('en-IN'));
+
+// ++++++++++++++++++ Maths +++++++++++++++++++++++++++++++
+// console.log(Math);
+// console.log(Math.abs(-4));
+// console.log(Math.round(7.5)); //8
+// console.log(Math.ceil(7.2));// 8
+// console.log(Math.floor(7.8));//7
+// console.log(Math.min(3,5,2,8,9));
+// console.log(Math.max(3,5,2,8,9));
+
+console.log(Math.random());
+console.log((Math.random()*10)+1);
+
+console.log(Math.floor(Math.random()*10) + 1);
+
+const min = 10;
+const max = 20;
+
+console.log(Math.floor(Math.random()* (max-min+1)) + min);
