b17e33a321e3f87117ae18d0e69cbc9c51438961
Nikhil_Desai <112999524+desainikhil03@users.noreply.github.com>
2024-07-17 14:52:47 +0000

    objects in js part one

--- 02_basics/03_objects.js
+++ 02_basics/03_objects.js
@@ -2,9 +2,13 @@
 //Object.create // object create through constructor
 
 // object literals
+
+const mySym = Symbol("key1")
+
 const JsUser = {
     name:"nikhil",
     "full_name":"nikhil desai",
+    [mySym]:"myKey1",
     age:21,
     lacation:"kolhapur",
     email:"nikhil@google.com",
@@ -12,6 +16,25 @@ const JsUser = {
     lastLoginDays:["Monday","Tuesday"]
 }
 
-console.log(JsUser.age); 
-console.log(JsUser["email"]);
-console.log(JsUser["full_name"]);
+// console.log(JsUser.age); 
+// console.log(JsUser["email"]);
+// console.log(JsUser["full_name"]);
+// console.log(JsUser[mySym]);
+// console.log(typeof JsUser[mySym]);
+
+// JsUser.email = "nikhil@gpt.com"
+// Object.freeze(JsUser)
+// JsUser.email = "nikhil@microsoft.com"
+// console.log(JsUser.email);
+// console.log(JsUser);
+
+JsUser.greetings = function(){
+    console.log("Hello  JS user");
+}
+
+JsUser.greetingsTwo = function(){
+    console.log(`Hello  JS user , ${this.name}`);
+}
+
+console.log(JsUser.greetings());
+console.log(JsUser.greetingsTwo());
