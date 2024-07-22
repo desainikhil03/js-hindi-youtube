4059830d92b2d443b48f11a76a06db4b4bdb5aaa
Nikhil_Desai <112999524+desainikhil03@users.noreply.github.com>
2024-07-22 18:05:23 +0000

    switch control flow in js

---
+++ 04_control_flow/switch.js
@@ -0,0 +1,53 @@
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+const month = 3
+
+switch (month) {
+    case 1:
+        console.log("january");
+        break;
+    case 2:
+        console.log("february");
+        break;
+    case 3:
+        console.log("march");
+        break;
+    case 4:
+        console.log("april");
+        break;
+    case 5:
+        console.log("may");
+        break;
+    case 6:
+        console.log("june");
+        break;
+    case 7:
+        console.log("july");
+        break;
+    case 8:
+        console.log("august");
+        break;
+    case 9:
+        console.log("september");
+        break;
+    case 10:
+        console.log("octobmerr");
+        break;
+    case 11:
+        console.log("nohember");
+        break;
+    case 12:
+        console.log("december");
+        break;
+
+    default:
+        console.log("enter 1-12 num");
+        break;
+}
