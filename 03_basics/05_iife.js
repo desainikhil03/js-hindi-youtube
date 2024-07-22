2af7082cb41303f52df15d90c48528334916cef8
Nikhil_Desai <112999524+desainikhil03@users.noreply.github.com>
2024-07-22 13:32:38 +0000

    Immediately Invoked Function Expressions (IIFE) in js

--- 03_basics/05_iife.js
+++ 03_basics/05_iife.js
@@ -5,7 +5,7 @@
 })();
 
 // iife syntax ()(); in first () we write function df and second () we call function
-// global scope ke polution se problem hoti kai baar us global scope ke jo variable hain , 
+// global scope ke polution se problem hoti kai baar , us global scope ke jo variable hain , 
 //jo bhi vha decleration hain, uske polution ko htane ke liye hamne iife ka use kiya
 
 ( (name) =>  {  // unnamed IIFE
