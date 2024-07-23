4218fde8914f589a030e74d7c9c03bba285e7a9a
Nikhil_Desai <112999524+desainikhil03@users.noreply.github.com>
2024-07-23 18:47:04 +0000

    iterations in js loops

---
+++ 05_iterations/one.js
@@ -0,0 +1,19 @@
+// for loop 
+
+// const array = [1,2,3,4,5]
+
+// for (let i = 0; i < array.length; i++) {
+//     const element = array[i];
+//     console.log(element);
+    
+// }
+
+for (let i = 0; i <= 10; i++) {
+    const element = i;
+    if (element == 5) {
+        //console.log("5 is best number");
+    }
+    //console.log(i);
+}
+
+//console.log(element); // error generate
