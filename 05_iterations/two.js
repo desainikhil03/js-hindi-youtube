fd553d0cec2eb22872b987d3d22f4f7d17b0eff7
Nikhil_Desai <112999524+desainikhil03@users.noreply.github.com>
2024-07-24 16:09:03 +0000

    iterations in js while and do while loop

---
+++ 05_iterations/two.js
@@ -0,0 +1,22 @@
+
+// let index = 0;
+// while (index<=10) {
+//     console.log(`value of index is ${index}`);
+//     index+=2
+// }
+
+// let myArray = ["arjun" , "karna" , "krishna" , "bhishma" ]
+
+// let arr = 0;
+// while (arr < myArray.length) {
+//     console.log(`calue is ${myArray[arr]}`);
+//     arr++;
+// }
+
+
+let score = 92;
+
+do {
+    console.log(`rohit sharma score ${score} runs`);
+    score++;
+} while (score < 80);
