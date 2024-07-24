9769a00ba4404d65a45c1e3afcd07ceadb6be717
Nikhil_Desai <112999524+desainikhil03@users.noreply.github.com>
2024-07-24 07:42:14 +0000

    iterations in js for loop

--- 05_iterations/one.js
+++ 05_iterations/one.js
@@ -17,3 +17,37 @@ for (let i = 0; i <= 10; i++) {
 }
 
 //console.log(element); // error generate
+
+for (let i = 1; i <=10; i++) {
+    //console.log(`Table of  ${i} : `);
+    for (let j = 1; j <=10; j++) {
+        //console.log(`Inner loop value ${j}`);
+        //console.log(`${i} * ${j} = ${i*j}`);
+    }
+    
+}
+
+let myArray = ["arjun" , "karna" , "krishna" , "bhishma" ]
+
+for (let index = 0; index < myArray.length; index++) {
+    const element = myArray[index];
+    //console.log(element);
+}
+
+// for (let index = 1; index <= 20; index++) {
+//     if(index == 5){
+//         console.log(`5 is Decteded`);
+//         break
+//     }
+//     console.log(`value of index is ${index}`);
+    
+// }
+
+for (let index = 1; index <= 20; index++) {
+    if(index == 5){
+        console.log(`5 is Decteded`);
+        continue
+    }
+    console.log(`value of index is ${index}`);
+    
+}
